b38026bbcf800cb740c1b296422aaa7b86894bc8
Collin Scherbert <cscherbert89@gmail.com>
2019-07-25 15:29:16 -0500

    Created Animal Index page

--- HumaneCore/Controllers/AnimalController.cs
+++ HumaneCore/Controllers/AnimalController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using HumaneCore.Data.Interfaces;
 using HumaneCore.Data.Models;
+using HumaneCore.Models.Animal;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HumaneCore.Controllers
@@ -19,8 +20,23 @@ namespace HumaneCore.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Animal> animals = _animalService.GetAll();
-            return View();
+            IEnumerable<AnimalListingViewModel> animals = _animalService.GetAll()
+                .Select(animal => new AnimalListingViewModel
+                {
+                    Id = animal.Id,
+                    Name = animal.Name,
+                    Age = animal.Age,
+                    Gender = animal.Gender,
+                    Species = animal.Species,
+                    Color = animal.Color,
+                    Restriction = animal.Restriction,
+                    Media = animal.Media
+                });
+            var model = new AnimalIndexViewModel
+            {
+                AnimalList = animals
+            };
+            return View(model);
         }
     }
 }
---
+++ HumaneCore/Models/Animal/AnimalIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumaneCore.Models.Animal
+{
+    public class AnimalIndexViewModel
+    {
+        public IEnumerable<AnimalListingViewModel> AnimalList { get; set; }
+    }
+}
